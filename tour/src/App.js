35b8d75c1839d98573eaf0f1d1c6498ee02b0c0c
Deepak <bbaz2deepak@gmail.com>
2022-09-06 12:03:26 +0530

    some changes

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "react-multi-carousel/lib/styles.css";
 import { Routes,Route } from 'react-router-dom'
 import About from './components/About';
@@ -8,8 +8,29 @@ import Navbar from './components/Navbar';
 import Tours from './components/Tours';
 
 const App = () => {
+  const [theme,setTheme] = useState(null)
+
+  useEffect(() => {
+    if(window.matchMedia(('prefers-color-scheme: dark')).matches) {
+
+      setTheme('dark');
+
+    }else{
+      setTheme('Light');
+    }
+
+  },[])
+
+  useEffect(() => {
+    if(theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    }else{
+      document.documentElement.classList.remove('dark');
+
+    }
+  })
   return (
-    <>
+    <div className='bg-white dark:bg-slate-900'>
     <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
@@ -17,7 +38,7 @@ const App = () => {
       <Route path='/tour' element={<Tours />} />
       <Route path='/contact' element={<Contact />} />
     </Routes>
-    </>
+    </div>
   )
 }
 
--- tour/tailwind.config.js
+++ tour/tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode:'class',
   theme: {
     extend: {
       boxShadow: {
         box: "8px 16px 90px -3px rgba(0,0,0,0.75)",
       },
-      backgroundImage: {
-        'hero': "url('https://preview.colorlib.com/theme/tralive/assets/img/hero/h1_hero1.png.webp')",
-      }
+     
     },
   },
   plugins: [],
